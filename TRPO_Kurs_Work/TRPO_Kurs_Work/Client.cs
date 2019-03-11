96eec619c62959d03824c40d6bebb8ab983f40d5
Incredibox <nickitabikow@yandex.ru>
2019-03-11 20:31:29 +0300

    Add project

---
+++ TRPO_Kurs_Work/TRPO_Kurs_Work/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO_Kurs_Work
+{
+    class Client
+    {
+        String name;
+        String phoneNumber;
+
+        Client(String name,String phoneNumber)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+        public string getPhone()
+        {
+            return phoneNumber;
+        }
+    }
+}
---
+++ TRPO_Kurs_Work/TRPO_Kurs_Work/ClientStorage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TRPO_Kurs_Work
+{
+    class ClientStorage
+    {
+        private static ClientStorage storage;        
+        ClientStorage()
+        {}
+        public static ClientStorage getStorage()
+        {
+            if (storage == null)
+                storage = new ClientStorage();
+            return storage;
+        }
+
+        IDictionary<String, Client> clientStorage = new Dictionary<String, Client>();
+
+        void PutClient(Client client)
+        {
+            if (!clientStorage.ContainsKey(client.getPhone()))
+                clientStorage.Add(client.getPhone(), client);
+        }
+
+        IDictionary<String, Client> GetStotage()
+        {
+            return clientStorage;
+        }
+        //New comment
+    }
+}
